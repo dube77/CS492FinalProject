23071a0a6f11a22c9b2b516902f8f91f3d4ad2d1
Jeremy Dube <dub377@gmail.com>
2018-04-15 11:40:23 -0400

    Added Account Class Added ReadFileToArray Method Added Test.txt to test read input

---
+++ src/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    private String website;
+    private String username;
+    private String password;
+
+    public Account(String website, String username, String password){
+        this.website = website;
+        this.username = username;
+        this.password = password;
+    }
+}
--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -1,2 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PasswordManager {
+    public static void main(String[] args) throws IOException {
+        PasswordManager pm = new PasswordManager();
+        ArrayList<String> test = pm.ReadFileToArray("Test");
+        for (String s : test) {
+            System.out.println(s);
+        }
+    }
+
+    private ArrayList<String> ReadFileToArray(String path) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            ArrayList<String> lines = new ArrayList<>();
+            for (String line; (line = br.readLine()) != null; ){
+                lines.add(line);
+            }
+            return lines;
+        }
+    }
 }
